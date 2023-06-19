a8f15fae538de32d6b2bc78891fbe73f6a6f8f05
marcelsguima <marcelsguima@gmail.com>
2023-06-19 12:19:29 -0300

    3.2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
+
+    _, file_extension = os.path.splitext(path_file)
+    if file_extension != ".txt":
+        print("Formato inválido", file=sys.stderr)
+        return []
+
+    with open(path_file, "r") as file:
+        lines = file.readlines()
+
+    return [line.strip() for line in lines]
